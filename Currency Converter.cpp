2a61d7650f2b8807a832ce42df87e1f6ee2a42bc
Chris B <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-14 22:26:20 -0400

    JPY might work, still testing

--- Currency Converter.cpp
+++ Currency Converter.cpp
@@ -9,8 +9,9 @@ double num1;
 double num2;
 char Currency;
 double EURExchange = 1.0756;
-double MXNExchange =0.0537;
-double GBPExchange =1.2174;
+double MXNExchange = 0.0537;
+double GBPExchange = 1.2174;
+double JPYExchange = 0.0075;
 
 int main(){
 
@@ -18,7 +19,7 @@ std::cout <<"Enter currency ammount in usd: ";
 std::cin >> num1;
 
 std::cout <<"What Currency would you like to convert to? \n";
-std::cout <<"($-USD, &-EUR, #-MXN, !-GBP): ";
+std::cout <<"($-USD, &-EUR, #-MXN, !-GBP, *-JPY): ";
 std::cin >>Currency;
 
 switch (Currency){
@@ -42,6 +43,10 @@ case '!':
     std::cout << "GBP: " << result << '\n';
     break;
 
+case '*':
+    result = num1 * JPYExchange;
+    std::cout << "JPY: " << result << '\n';
+    break;
 }
 
 return 0;
