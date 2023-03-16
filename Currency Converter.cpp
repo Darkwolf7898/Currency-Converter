58c94a62b037d256b105b591fc30b58ad79c86b7
Chris B <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-16 11:29:38 -0400

    fixed some things and added CAD

--- Currency Converter.cpp
+++ Currency Converter.cpp
@@ -12,14 +12,15 @@ double EURExchange = 1.0756;
 double MXNExchange = 0.0537;
 double GBPExchange = 1.2174;
 double JPYExchange = 0.0075;
+double CADExchange = 0.7267;
 
 int main(){
 
-std::cout <<"Enter currency ammount in usd: ";
+std::cout <<"Enter currency ammount: ";
 std::cin >> num1;
 
 std::cout <<"What Currency would you like to convert to? \n";
-std::cout <<"($-USD, &-EUR, #-MXN, !-GBP, *-JPY): ";
+std::cout <<"($-USD, &-EUR, #-MXN, !-GBP, *-JPY, @-CAD): ";
 std::cin >>Currency;
 
 switch (Currency){
@@ -30,29 +31,29 @@ case '$':
     		
 case '&':
     result = num1 * EURExchange;
-    std::cout << "EUR: " << result << '\n';
+    std::cout << "USD: " << result << '\n';
     break;
         		
 case '#':
     result = num1 * MXNExchange;
-    std::cout << "MXN: " << result << '\n';
+    std::cout << "USD: " << result << '\n';
     break;
 
 case '!':
     result = num1 * GBPExchange;
-    std::cout << "GBP: " << result << '\n';
+    std::cout << "USD: " << result << '\n';
     break;
 
 case '*':
     result = num1 * JPYExchange;
-    std::cout << "JPY: " << result << '\n';
+    std::cout << "USD: " << result << '\n';
     break;
 
-/* case '':
-    result = 0;
-    std::cout << ": " << result << '\n';
+ case '@':
+    result = num1 * CADExchange;
+    std::cout << "USD: " << result << '\n';
     break;
-    
+/*    
 case '':
     result = 0;
     std::cout << ": " << result << '\n';
