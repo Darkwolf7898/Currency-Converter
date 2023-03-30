9723c15e8cee2af6c00246b5c3044361deee69c0
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-30 13:09:58 -0400

    Official Release v1.0.0

---
+++ include/const.h
@@ -0,0 +1,8 @@
+double result;
+double num1;
+char Currency;
+double EURExchange = 1.0756;
+double MXNExchange = 0.0537;
+double GBPExchange = 1.2174;
+double JPYExchange = 0.0075;
+double CADExchange = 0.7267;
